2ba47e6912f7b57f73d69c4fedb95c6606ce5433
DomoKrch <kyndomo9@gmail.com>
2020-05-10 01:07:22 +0600

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ client.on('message', msg => {
   if (msg.content === "nigga" || msg.content === "Nigga" ||
       msg.content === "nigger" || msg.content === "Nigger" ||
       msg.content === "fag" || msg.content === "Fag" ||
-      msg.content === "faggot" || msg.content === "Faggot" ||)
+      msg.content === "faggot" || msg.content === "Faggot")
   {
     msg.reply('ok');
   }
